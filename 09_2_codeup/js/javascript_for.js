e9ab8197794a1dd8f44c4d8541d0c05043d8fe66
dokwonjae <dobear7@naver.com>
2023-09-06 23:28:14 +0900

    11

--- 09_2_codeup/js/javascript_for.js
+++ 09_2_codeup/js/javascript_for.js
@@ -14,51 +14,184 @@
 // alert(nul)
 
 // 1253
-let a = prompt().split(" ")
-let nul =""
-if(a[0]<a[1]){
-for(let i=a[0];i<=a[1];i++){
-    nul += i + " "
-}
-}else{
-    for(let i=a[1];i<=a[0];i++){
-    nul += i + " "
-    }
-}
-alert(nul)
+// let a = prompt().split(" ")
+// let nul =""
+// if(a[0]<a[1]){
+// for(let i=a[0];i<=a[1];i++){
+//     nul += i + " "
+// }
+// }else{
+//     for(let i=a[1];i<=a[0];i++){
+//     nul += i + " "
+//     }
+// }
+// alert(nul)
 
 // 1254
+// let a = prompt().split(" ")
+// let b =a[0].charCodeAt(0)
+// let c =a[1].charCodeAt(0)
+// let nul =""
+// for (let i = b; i <= c; i++) {
+//   nul += String.fromCodePoint(i) + " " ;
+// }
+// alert(nul)
 
 // 1255
+// let a = prompt().split(" ")
+// let b = a.map(Number)
+// let nul = ""
+// for(let i =b[0];i<=b[1];i+=0.01){
+//     nul += i.toFixed(2) +" "
+// }
+// alert(nul)
 
 // 1256
+// let a = Number(prompt());
+// let nul = ""
+// for(let i =1;i<=a;i++){
+//     nul += "*"
+// }
+// alert(nul)
 
 // 1257
+// let a = prompt().split(" ").map(Number)
+// let nul =""
+// for(a[0];a[0]<=a[1];a[0]++){
+//     if(a[0]%2==1){
+//     nul += a[0] + " "
+//     }
+// }
+// alert(nul)
 
 // 1258
+// let a = prompt()
+// let sum = 0;
+// for(let i =1; i<=a; i++){
+//     sum += i;
+// }
+// alert(sum)
 
 // 1259
+// let a = Number(prompt())
+// let sum = 0;
+// for(let i = 1;i<=a;i++){
+//     if(i%2==0){
+//         sum+=i;
+//     }
+// }
+// alert(sum)
 
 // 1260
+// let a = prompt().split(" ").map(Number)
+// let sum = 0;
+// for(let i = a[0];i<=a[1];i++){
+//     if(i%3==0){
+//         sum+=i;
+//     }
+// }
+// alert(sum)
 
 // 1261
+// let a = prompt().split(" ").map(Number)
+// let nul = ""
+// for(let i = 0; i<a.length ;i++){
+// if(a[i]%5==0){
+//     nul +=a[i]
+//     break
+// }
+// }
+// alert(nul)
 
 // 1265
+// let a = Number(prompt())
+// let nul = ""
+// for(let i=1;i<10;i++){
+//     nul += 3 +"*"+i+"="+3*i + "\n"
+// }
+// alert(nul)
 
 // 1266
+// let a = Number(prompt());
+// let b = prompt().split(" ").map(Number)
+// let sum =0;
+// for(let i=0;i<a;i++){
+// sum += b[i]
+// }
+// alert(sum)
 
 // 1267
+// let a = Number(prompt());
+// let b = prompt().split(" ").map(Number)
+// let sum =0;
+// for(let i=0;i<a;i++){
+//     if(b[i]%5==0){
+//         sum += b[i]
+//     }
+// }
+// alert(sum)
 
 // 1268
+// let a = Number(prompt());
+// let b = prompt().split(" ").map(Number)
+// let sum =0;
+// for(let i=0;i<a;i++){
+//     if(b[i]%2==1){
+//         sum ++
+//     }
+// }
+// alert(sum)
 
 // 1269
+// let num = prompt().split(" ").map(Number)
+// let a = num[0]
+// let b = num[1]
+// let c = num[2]
+// let n = num[3]
+// let sum = a
+// for(let i=2; i<=n; i++){
+// sum = sum * b + c
+// }
+// alert(sum)
 
 // 1270
+// let a = Number(prompt())
+// let cnt=0
+// for(let i=1; i<=a;i++){
+//     if(i%10==1){
+//     cnt++
+//     }
+// }
+// alert(cnt)
 
 // 1271
+// let a = Number(prompt());
+// let b = prompt().split(" ").map(Number)
+// let max =0;
+// for(let i=0;i<a;i++){
+//     if(max<b[i]){
+//         max = b[i]
+//     }
+// }
+// alert(max)
 
 // 1272
+// let a = prompt().split(" ").map(Number);
+// let sum = 0;
+// let start = 1;
+// let max = Math.max(a[0], a[1]);
+// let min = Math.min(a[0], a[1]);
 
+// for (let i = 0; i <= max; i++) {
+//   if (i % 2 == 0) {
+//     a[i] = start++
+//   } else {
+//     a[i]= a[i-1] * 10;
+//   }
+//   sum = a[min-1]+a[max-1]
+// }
+
+// alert(sum);
 // 1273
 
 // 1274
